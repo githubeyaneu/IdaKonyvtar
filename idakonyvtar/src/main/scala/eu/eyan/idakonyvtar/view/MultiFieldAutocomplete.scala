361be7d02d20d28629b7289939f7faef7bd880dc
githubeyaneu <github@eyan.eu>
2016-12-17 07:36:02 +0100

    Merge

--- idakonyvtar/src/main/scala/eu/eyan/idakonyvtar/view/MultiFieldAutocomplete.scala
+++ idakonyvtar/src/main/scala/eu/eyan/idakonyvtar/view/MultiFieldAutocomplete.scala
@@ -2,22 +2,25 @@ package eu.eyan.idakonyvtar.view
 
 import eu.eyan.util.swing.Autocomplete
 import eu.eyan.log.Log
+import eu.eyan.util.swing.JTextFieldAutocomplete
+import eu.eyan.log.Log
+import eu.eyan.util.swing.JTextFieldPlus.JTextFieldPlusImplicit
 
-class MultiFieldAutocomplete(columnName: String, hintText: String) extends MultiField[String, Autocomplete](columnName) {
+class MultiFieldAutocomplete(columnName: String, hintText: String, noItemsFoundText: String) extends MultiField[String, JTextFieldAutocomplete](columnName) {
 
   var autocompleteList: List[String] = List()
 
   Log.debug(columnName)
 
-  protected def addFieldEditListener(editor: Autocomplete, listener: FieldEditListener[Autocomplete]): Unit =
+  protected def addFieldEditListener(editor: JTextFieldAutocomplete, listener: FieldEditListener[JTextFieldAutocomplete]): Unit =
     editor.addKeyReleasedListener(x => if (editor.getText.nonEmpty) listener.fieldEdited(editor))
 
-  protected def getEditor(): Autocomplete =
-    new Autocomplete().setValues(autocompleteList).setHintText(hintText)
+  protected def getEditor(): JTextFieldAutocomplete =
+    new JTextFieldAutocomplete().setValues(autocompleteList).setHintText(hintText).setNoItemsFoundText(noItemsFoundText)
 
-  protected def getValue(editor: Autocomplete): String = if (editor.getText.equals("")) null else editor.getText
+  protected def getValue(editor: JTextFieldAutocomplete): String = if (editor.getText.equals("")) null else editor.getText
 
-  protected def setValueInEditor(editor: Autocomplete, value: String): Unit = {
+  protected def setValueInEditor(editor: JTextFieldAutocomplete, value: String): Unit = {
     editor.setText(value)
     Log.debug(editor.getName + " " + value)
   }
